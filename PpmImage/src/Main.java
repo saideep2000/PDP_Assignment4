c2d0789bea51f69b5644bd170e0cbf55192ec441
AJAYINAVOLU <ajayreddy6109@gmail.com>
2023-03-16 20:09:37 -0400

    Add files via upload

--- PpmImage/src/Main.java
+++ PpmImage/src/Main.java
@@ -11,16 +11,16 @@ import java.io.FileNotFoundException;
  * This is the main class of the program used to run the program through the command interface.
  */
 public class Main {
-    /**
-     * The main method calls the controller and sends data to the model.
-     *
-     * @param args is command line arguments.
-     * @throws FileNotFoundException when the file is not found, it throws an exception.
-     */
-    public static void main(String[] args) throws FileNotFoundException {
-        ImeModel imeM = new ImeModelImpl();
-        ImeView imeV = new ImeViewImpl(System.out);
-        ImeController imeC = new ImeControllerImpl(imeM, imeV, System.in);
-        imeC.ppmgo();
-    }
+  /**
+   * The main method calls the controller and sends data to the model.
+   *
+   * @param args is command line arguments.
+   * @throws FileNotFoundException when the file is not found, it throws an exception.
+   */
+  public static void main(String[] args) throws FileNotFoundException {
+    ImeModel imeM = new ImeModelImpl();
+    ImeView imeV = new ImeViewImpl(System.out);
+    ImeController imeC = new ImeControllerImpl(imeM, imeV, System.in);
+    imeC.ppmgo();
+  }
 }
