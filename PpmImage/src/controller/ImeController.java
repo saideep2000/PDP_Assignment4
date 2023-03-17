e6a540a60a7074a0ad1bbc8e774df627bb3c0eba
AJAYINAVOLU <ajayreddy6109@gmail.com>
2023-03-16 20:07:49 -0400

    Add files via upload

--- PpmImage/src/controller/ImeController.java
+++ PpmImage/src/controller/ImeController.java
@@ -6,10 +6,10 @@ import java.io.FileNotFoundException;
  * This interface represents a set of operations in the controller of the program.
  */
 public interface ImeController {
-    /**
-     * This method will call the necessary private methods by analyzing the input of the user.
-     *
-     * @throws FileNotFoundException is thrown when the required file is not found.
-     */
-    void ppmgo() throws FileNotFoundException;
+  /**
+   * This method will call the necessary private methods by analyzing the input of the user.
+   *
+   * @throws FileNotFoundException is thrown when the required file is not found.
+   */
+  void ppmgo() throws FileNotFoundException;
 }
